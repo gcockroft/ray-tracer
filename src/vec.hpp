a5c3a53ae0ccdb7d977f0d8c7bb5c65787a61566
gcockroft <77020027+gcockroft@users.noreply.github.com>
2022-12-20 13:24:31 -0500

    Using color, vec -> vec3, unit_vector()

--- src/vec.hpp
+++ src/vec.hpp
@@ -1,48 +1,48 @@
-#ifndef VEC
-#define VEC
+#ifndef vec3
+#define vec3
 
 #include <cmath>
 #include <iostream>
 
-class vec {
+class vec3 {
   public:
     float x;
     float y;
     float z;
 
-    vec() {
+    vec3() {
       x = 0;
       y = 0;
       z = 0;
     }
 
-    vec(float _x, float _y, float _z) {
+    vec3(float _x, float _y, float _z) {
       x = _x;
       y = _y;
       z = _z;
     }
 
-    vec operator-() const {return vec(-x, -y, -z);}
+    vec3 operator-() const {return vec3(-x, -y, -z);}
 
-    vec operator+=(const vec &v) {
+    vec3 operator+=(const vec3 &v) {
       this->x += v.x;
       this->y += v.y;
       this->z += v.z;
     }
 
-    vec operator-=(const vec &v) {
+    vec3 operator-=(const vec3 &v) {
       this->x -= v.x;
       this->y -= v.y;
       this->z -= v.z;
     }
 
-    vec operator*=(const float s) {
+    vec3 operator*=(const float s) {
       this->x *= s;
       this->y *= s;
       this->z *= s;
     }
 
-    vec operator/=(const float s) {
+    vec3 operator/=(const float s) {
       return *this *= (1.0f/s);
     }
 
@@ -52,44 +52,49 @@ class vec {
     }
 };
 
-inline std::ostream& operator<<(std::ostream &out, const vec &v) {
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
     return out << v.x << ' ' << v.y << ' ' << v.z;
 }
 
-inline vec operator+(const vec &u, const vec &v) {
-  return vec(u.x + v.x, u.y+v.y, u.x+v.z);
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+  return vec3(u.x + v.x, u.y+v.y, u.x+v.z);
 }
 
-inline vec operator-(const vec &u, const vec &v) {
-  return vec(u.x - v.x, u.y-v.y, u.x-v.z);
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+  return vec3(u.x - v.x, u.y-v.y, u.x-v.z);
 }
 
-inline vec operator*(const vec &u, const vec &v) {
-  return vec(u.x * v.x, u.y*v.y, u.x*v.z);
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+  return vec3(u.x * v.x, u.y*v.y, u.x*v.z);
 }
 
-inline vec operator*(const vec &u, const float s) {
-  return vec(u.x * s, u.y * s, u.z * s);
+inline vec3 operator*(const vec3 &u, const float s) {
+  return vec3(u.x * s, u.y * s, u.z * s);
 }
 
-inline vec operator*(const float s, const vec &u) {
+inline vec3 operator*(const float s, const vec3 &u) {
   return u * s;
 }
 
-inline vec operator/(const vec &u, const vec &v) {
-  return vec(u.x / v.x, u.y/v.y, u.x/v.z);
+inline vec3 operator/(const vec3 &u, const vec3 &v) {
+  return vec3(u.x / v.x, u.y/v.y, u.x/v.z);
 }
 
-inline float dot(const vec &u, const vec &v) {
+inline float dot(const vec3 &u, const vec3 &v) {
   return u.x * v.x + u.y * v.y + u.z * v.z;
 }
 
-inline vec cross(const vec &u, const vec &v) {
+inline vec3 cross(const vec3 &u, const vec3 &v) {
   float x = u.y*v.z - u.z * v.y;
   float y = -(u.x * v.z - u.z*v.x);
   float z = u.x*v.y - u.y*v.x;
-  return vec(x,y,z);
+  return vec3(x,y,z);
 }
 
+inline vec3 unit_vector(vec3 v) {
+  return v / v.length();
+}
+
+using color = vec3;
 
 #endif
