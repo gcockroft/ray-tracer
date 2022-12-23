aa4e6c0e7f472bf7bd43b51608ec39ac72a295f1
Ethan Fox <efox3987@gmail.com>
2022-12-23 11:10:07 -0500

    Sphere in progress

---
+++ src/sphere.cpp
@@ -0,0 +1,10 @@
+#include "sphere.hpp"
+
+sphere::sphere(float c, float r) {
+    center = c;
+    radius = r;
+}
+
+float sphere::test_intersection(ray r) {
+    
+}
--- src/sphere.hpp
+++ src/sphere.hpp
@@ -1,4 +1,18 @@
 #ifndef SPHERE
 #define SPHERE
 
+#include "shape.hpp"
+#include "vec3.hpp"
+#include "ray.hpp"
+
+class sphere : public shape {
+public: 
+	float center;
+	float radius;
+
+	sphere(float c, float r);
+
+	float test_intersection(ray r);
+};
+
 #endif
